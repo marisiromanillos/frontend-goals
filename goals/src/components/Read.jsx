6cdd915e7e6cf58a0c110dd61de7901d8203bc6c
Marisi Romanillos <marisiromanillos@Marisis-MBP.lan>
2024-01-24 18:25:27 +0000

    need to fix the id

--- goals/src/components/Read.jsx
+++ goals/src/components/Read.jsx
@@ -1,4 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+
 const Read = () => {
-  return <></>;
+  const [data, setData] = useState([]);
+  const { id } = useParams();
+
+  const getGoalsData = async () => {
+    try {
+      const response = await axios.get(`http://localhost:6001/goals/${id}`);
+      const goalsData = response.data.data; // Access the 'data' property
+
+      if (Array.isArray(goalsData)) {
+        setData(goalsData);
+        console.log(goalsData);
+      } else {
+        console.error("Data is not an array:", goalsData);
+        setData([]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getGoalsData();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <h1>Goal {id}</h1>
+        <Link to="/">Back</Link>
+        {data.map((goal) => {
+          return (
+            <ul>
+              <li>
+                <b>ID:</b>
+                {goal["id"]}
+              </li>
+              <li>
+                <b>Goal</b>
+                {goal["goal"]}
+              </li>
+              <li>
+                <b>Call to action</b>
+                {goal["call_to_action"]}
+              </li>
+            </ul>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 export default Read;
